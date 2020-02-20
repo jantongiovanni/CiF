4b11e635fcc3d833a92ffe95b9f20ad8b2ebd171
Joe Anton <janton153@gmail.com>
2020-02-20 15:26:12 -0800

    Android Neumorphism

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NeomorphBox } from 'react-native-neomorph-shadows';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NeomorphBox
+        swapShadowLevel // <- change zIndex of each shadow color
+        style={{
+          shadowRadius: 10,
+          borderRadius: 25,
+          backgroundColor: '#efeeee',
+          shadowColor: '#cbcaca',
+          width: 150,
+          height: 150,
+        }}
+      >
+      </NeomorphBox>
     </View>
   );
 }
@@ -12,7 +24,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#efeeee',
     alignItems: 'center',
     justifyContent: 'center',
   },
